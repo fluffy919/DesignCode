e0d5112c05ace7466f0127a321e1d6d262fcf18c
mobileANDweb <mobiledeveloper112075@gmail.com>
2021-03-17 07:36:57 -0700

    Warning fix

---
+++ DesignCode/TabBar.swift
@@ -0,0 +1,41 @@
+//
+//  TabBar.swift
+//  DesignCode
+//
+//  Created by David S on 7/15/19.
+//  Copyright © 2019 David S. All rights reserved.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+   var body: some View {
+      TabView {
+         Home().tabItem {
+            Image("IconHome")
+            Text("Home")
+         }
+         .tag(1)
+         ContentView().tabItem {
+            Image("IconCards")
+            Text("Certificates")
+         }
+         .tag(2)
+         Settings().tabItem {
+            Image("IconSettings")
+            Text("Settings")
+         }
+         .tag(3)
+      }
+      .edgesIgnoringSafeArea(.top)
+   }
+}
+
+#if DEBUG
+struct TabBar_Previews: PreviewProvider {
+   static var previews: some View {
+      TabBar()
+//         .environment(\.colorScheme, .dark)
+   }
+}
+#endif
